b9483744dbe7c57d00d8d4caaf0b9cfad07012c3
Arshdeep Kaur <sainiarshdeep114@gmail.com>
2025-06-25 19:26:47 +0530

    jd

--- src/Components/AboutCEO.jsx
+++ src/Components/AboutCEO.jsx
@@ -11,7 +11,7 @@ export default function AboutCEO()
                 <div className="cc-sub">The Vision Behind Premium Real Estate in South Delhi</div>
                 <div className="cc-super">Meet Naeem Ahmed</div>
                 <div className="cc-content">
-                Naheem Ahmed, the driving force behind our real estate journey, brings over 5 years of hands-on experience in crafting premium living spaces. With a strong focus on trust, quality construction, and timely delivery, he has set new benchmarks in the South Delhi real estate market. Specializing in DLF Farms and the Chhattarpur area, Naheem and his team are committed to delivering elegant, well-planned homes that reflect both luxury and reliability. What started as a vision has now grown into a trusted name for those seeking excellence in every square foot.
+                Naeem Ahmed, the driving force behind our real estate journey, brings over 5 years of hands-on experience in crafting premium living spaces. With a strong focus on trust, quality construction, and timely delivery, he has set new benchmarks in the South Delhi real estate market. Specializing in DLF Farms and the Chhattarpur area, Naeem and his team are committed to delivering elegant, well-planned homes that reflect both luxury and reliability. What started as a vision has now grown into a trusted name for those seeking excellence in every square foot.
                 </div>
             </div>
         </div>
