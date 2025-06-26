5d26435447eba3c7ae0b00c6f735fec501395719
Arshdeep Kaur <sainiarshdeep114@gmail.com>
2025-06-26 21:57:30 +0530

    ewj

--- src/Pages/Thankyou.jsx
+++ src/Pages/Thankyou.jsx
@@ -1,14 +1,15 @@
 import Footer from "../Components/Footer";
+import '../public/Thankyou.css'
 import ThankyouCompo from "../Components/ThankyouCompo";
 export default function Thankyou()
 {
     return(
-        <div className="Thankyou">
+        <>
         <ThankyouCompo/>
         <div className="thanks">
         <Footer/>
         </div>
-        </div>
+        </>
     )
 
 }
